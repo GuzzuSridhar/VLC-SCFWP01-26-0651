41b231ea401d1e1879c85b8ebd47541a1b0ce6ac
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-02 09:29:26 +0530

    append datatypes

--- javascript/node/basics/datatypes.js
+++ javascript/node/basics/datatypes.js
@@ -1,1 +1,21 @@
 let numberType = 42;
+let stringType = "Hello";
+let booleanType = false;
+let undefinedType; // any variable that is declared and no value assigned is "undefined"
+let nullType = null;
+let arrayType = [1, 2, 3];
+let objectType = { name: "John", age: 30 };
+let functionType = function (a, b) {};
+let symbolType = Symbol("unique");
+let bigIntType = 12345678901234567890;
+
+console.log("The type for Numbers is:" + typeof numberType);
+console.log("The type for Strings is:" + typeof stringType);
+console.log("The type for Booleans is:" + typeof booleanType);
+console.log("The type for undefined is:" + typeof undefinedType);
+console.log("The type for null is:" + typeof nullType);
+console.log("The type for arraytype is:" + typeof arrayType);
+console.log("The type for objecttype is:" + typeof objectType);
+console.log("The type for functiontype is:" + typeof functionType);
+console.log("The type for symboltype is:" + typeof symbolType);
+console.log("The type for bigIntType is:" + typeof bigIntType);
