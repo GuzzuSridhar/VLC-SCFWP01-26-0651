3f9a7bd214f1596ccbb864c483c73317ee4afbd9
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-09 15:17:04 +0530

    add logical operators

--- javascript/node/Conditions/logical_operators.js
+++ javascript/node/Conditions/logical_operators.js
@@ -11,3 +11,16 @@ if (salary > 5000 && dept === "Sales") {
   salary += 1000;
 }
 console.log(salary);
+
+// for every employee either in sales or  having current salary more than 5000, increment by 1000
+
+if (salary > 5000 || dept === "Sales") {
+  salary += 1000;
+}
+console.log(salary);
+
+// Not opearator
+let x = 10,
+  y = 20;
+console.log(x < y); // true
+console.log(!(x < y)); //false
