82be7ab6aaa4aebaba627e3d4d555ecc783066bc
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-23 13:14:30 +0530

    add colors

---
+++ javascript/node/color/colors.js
@@ -0,0 +1,5 @@
+// printing text on the command line in colors
+
+console.log("\x1b[31m This text is in red color \x1b[0m");
+console.log("\x1b[32m This text is in green color \x1b[0m");
+console.log("\x1b[31m This text is in red color \x1b[0m");
