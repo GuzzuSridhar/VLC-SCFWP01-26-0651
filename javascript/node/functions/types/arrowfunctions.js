b0365aa60aaab1da00ae35d71675615353655416
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-23 12:30:38 +0530

    add map arrow funcs

--- javascript/node/functions/types/arrowfunctions.js
+++ javascript/node/functions/types/arrowfunctions.js
@@ -18,3 +18,36 @@ square(10); // not consuming the return
 // simple arrow function with multiple arguments and return
 const add = (n, m) => n + m;
 console.log(add(10, 20));
+// equivivalent to
+// function add(n,m){return n+m;}
+
+// multiple statments
+const calculate = (...n) => {
+  let res = 0;
+  for (i = 0; i < n.length; i++) {
+    res += n[i];
+  }
+  return res;
+};
+
+console.log(calculate(10, 20, 30, 40));
+
+// string use case
+const up = (str) => str.toUpperCase();
+console.log(up("hello"));
+
+const salaries = [2000, 3000, 4000, 5000];
+const salIncrement = salaries.map((salary) => salary + 0);
+console.log(salIncrement);
+
+const totalSalarySpend = salaries.reduce(
+  (total, salary) => total + salary,
+  1000,
+);
+console.log(totalSalarySpend);
+
+const filterSalaries = salaries.filter((salary) => salary > 3000);
+console.log(filterSalaries);
+
+const cities = ["Delhi", "Singapore", "NY"];
+cities.forEach((city) => console.log(city));
