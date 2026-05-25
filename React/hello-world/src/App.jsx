8acc73767d0c6b21c47dbcddbd59853fef8c1705
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-25 16:46:57 +0530

    add helloworld React app

---
+++ React/hello-world/src/App.jsx
@@ -0,0 +1,12 @@
+import MainHeading from "./components/MainHeading"
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <MainHeading />
+    </>
+
+  )
+}
+export default App
---
+++ React/hello-world/src/components/MainHeading.jsx
@@ -0,0 +1,9 @@
+function MainHeading() {
+    return (
+        <>
+            <h1 className="mainHead">Hello World</h1>
+        </>
+    )
+}
+
+export default MainHeading
