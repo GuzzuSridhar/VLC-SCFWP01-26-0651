029b47566d1b85f44a3b100d9fc1b4f86733e684
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-06-03 19:28:47 +0530

    add window event listener

---
+++ React/hooks/window-width/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <div>{windowWidth}</div>
+    </>
+  );
+}
+
+export default App;
