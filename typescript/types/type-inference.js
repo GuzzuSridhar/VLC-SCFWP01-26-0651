d94d7d10db084b01c5702c64cfd9fba601b61ed3
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-06-30 17:33:19 +0530

    add inference

---
+++ typescript/types/type-inference.js
@@ -0,0 +1,7 @@
+// let city: string = "Singapore"
+var city = "Singapore"; // same as above but type is inferred from the value
+// city = 123 // compile time error
+console.log(city);
+var num = 100;
+// num = "xyz"; // compile time error
+console.log(num);
---
+++ typescript/types/type-inference.ts
@@ -0,0 +1,8 @@
+// let city: string = "Singapore"
+let city = "Singapore" // same as above but type is inferred from the value
+// city = 123 // compile time error
+console.log(city)   
+
+let num = 100;
+// num = "xyz"; // compile time error
+console.log(num)
