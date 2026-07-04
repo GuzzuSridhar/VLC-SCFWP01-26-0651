9e822bc2b29d511ae3495f2ebb59afe2efd19c25
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-07-04 13:32:24 +0530

    add directives

---
+++ Vue/conditional-render/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import ConditionalRender from './components/ConditionalRender.vue';
+
+</script>
+
+<template>
+  <ConditionalRender />
+</template>
---
+++ Vue/conditional-render/src/components/ConditionalRender.vue
@@ -0,0 +1,25 @@
+
+<script setup>
+import {ref } from "vue";
+
+// declare a variable using ref 
+ const isLoggedIn = ref(true)
+
+//  function to toggle the login state
+const toggleLogin = () => {
+    isLoggedIn.value = !isLoggedIn.value
+}
+ </script>
+
+ <template>
+    <div>
+        <!-- if the value of isLoggedIn is false the element is removed from DOM -->
+        <h1 v-if ="isLoggedIn" > Welcome Back User!</h1>
+        <h1 v-else> Please login to continue</h1>
+        <button @click = "toggleLogin"> {{ isLoggedIn ? "Logout" : "Login" }} </button>
+
+        <!-- v-show -->
+         <p v-show = "isLoggedIn">  paid content visible  </p>
+
+    </div>
+ </template>
