b24e4d06b9d723d2a56771c1f85d710608eb1844
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-07-07 16:56:17 +0530

    add counter

---
+++ Vue/counter/src/components/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+  <h2>The count is: {{ count }} </h2>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+  <button @click="reset">Reset</button>
+</div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// ref in vue is similar to useState in react
+ const count = ref(0);
+
+  const increment = () => count.value++
+  const decrement = () => count.value--
+  const reset = () => count.value = 0
+
+  // function decrement(){
+  //   count.value--;
+  // }
+  // function increment(){
+  //   count.value++;
+  // }
+
+    // function reset(){
+  //   count.value = 0;
+  // }
+</script>
+
+<style scoped>
+  div{
+    text-align: center;
+  }
+</style>
